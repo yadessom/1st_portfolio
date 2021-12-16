9b6f51806782fd1f1e939f3befa82c0b26800aab
yadess <90118673+yadessom@users.noreply.github.com>
2021-12-16 20:43:08 +0900

    start

---
+++ portfolio/js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    $('#fullpage').fullpage({
+		//options here
+        sectionsColor: ["#bf3f4a","#d94862","#2c3359","#3b8dbf","#7ed9d0","#5451A6","#2c3359","#999"],
+		anchors: ['home','about','web','mobile','responsive','graphic','contact'],
+		autoScrolling:true,
+		scrollHorizontally: true,
+		navigation:true,
+		// navigationPosition:'left'
+		afterLoad: function(anchorLink, index){
+			console.log("현재섹션=" + index);
+			
+		}
+		
+	});
+
+	// *************************************************************
+	$('.m_btn').click(function(){
+        $('.line').toggleClass('on');
+		$('.full_menu').toggleClass('on');
+		$('.blackout').toggleClass('on');
+    });
+	$('.blackout').mouseenter(function(){
+		$('.line').toggleClass('on');
+		$('.full_menu').toggleClass('on');
+		$('.blackout').toggleClass('on');
+	});
+	
+
+});
