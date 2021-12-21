1be4845d3943b017cd309ae9fc00bd01f17c815f
yadess <90118673+yadessom@users.noreply.github.com>
2021-12-21 22:01:31 +0900

    + sec mobile modi
    + sec mobile ani
    full menu close btn ani modi
    + sec contact modi

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(function(){
 		afterLoad: function(anchorLink, index){
 			console.log("현재섹션=" + index);
 			
-			if(index == 1){
+			if(index == 1 || index ==7){
 				$('.title').addClass('on');
 			}else{
 				$('.title').removeClass('on');
@@ -37,11 +37,21 @@ $(function(){
 			}
 			if(index == 4){
 				$('.mobile_back').addClass('on');
-				$('.mobile_box_01').addClass('on');
+				// $('.mobile_box_01').addClass('on');
+				$('.m_icon_01').addClass('on');
+				$('.m_icon_02').addClass('on');
+				$('.m_icon_03').addClass('on');
+				$('.m_icon_04').addClass('on');
 			}else{
 				$('.mobile_back').removeClass('on');
-				$('.mobile_box_01').removeClass('on');
+				// $('.mobile_box_01').removeClass('on');
+				$('.m_icon_01').removeClass('on');
+				$('.m_icon_02').removeClass('on');
+				$('.m_icon_03').removeClass('on');
+				$('.m_icon_04').removeClass('on');
 			}
+
+
 		}
 	});
 	// *************************************************************
@@ -50,19 +60,24 @@ $(function(){
 		$('.full_menu').toggleClass('on');
 		$('.blackout').toggleClass('on');
 		$('.title').show(1000);
+		$('.m_btn').toggleClass('on');
     });
 	$('.blackout').click(function(){
 		$('.line').toggleClass('on');
 		$('.full_menu').toggleClass('on');
 		$('.blackout').toggleClass('on');
 		$('.title').show(1000);
+		$('.m_btn').removeClass('on');
 	});
 	// *************************************************************
 	$('.circle_01').click(function(){
 		$('.title').fadeOut(200);
 	});
 	// *************************************************************
-	
+	$('.mobile_list li').mouseenter(function(){
+		let num = $(this).index();
+		$('.m_box_all').eq(num).addClass('on').siblings().removeClass('on');
+	});
 
 
 });
